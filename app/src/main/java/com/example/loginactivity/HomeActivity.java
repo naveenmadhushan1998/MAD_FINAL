203f172739232d94fdafdab68115e604df6780c6
Naveen Madhushan <naveenmadhushan@gmail.com>
2019-09-25 21:09:11 +0530

    DATABASE

---
+++ app/src/main/java/com/example/loginactivity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.loginactivity;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
